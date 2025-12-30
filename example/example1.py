da17a8b0ee45c9c705d3706e450cc6ed83b33055
Akishika, Issui <kshkss@users.noreply.github.com>
2025-12-30 17:02:13 +0900

    test: update example code

--- example/example1.py
+++ example/example1.py
@@ -49,12 +49,13 @@ def _(IDA, NamedTuple, jax, jnp):
         )
 
     def constraint(params: Params, t: jax.Array, stat: State) -> jax.Array:
-        return stat.x ** 2 - stat.y
+        return stat.x ** 2 - jnp.sum(stat.y)
 
     solver = IDA(deriv_fn=derivative, const_fn=constraint)
     params = Params(a=jnp.array(1.0))
-    u, v = solver.solve(params, jnp.linspace(0, 1, 11), State(y=jnp.array(1.0), x=jnp.array(1.0)))
-    return State, derivative, params, solver, u, v
+    u, v, clear_cache = solver.solve(params, jnp.linspace(0, 1, 11), State(y=jnp.array([2.0, 1.0]), x=jnp.sqrt(3.0)))
+    clear_cache()
+    return State, params, solver, u, v
 
 
 @app.cell
@@ -70,25 +71,11 @@ def _(v):
 
 
 @app.cell
-def _(State, jnp):
-    _x = State(y=jnp.ones(1), x=jnp.ones(1))
-    _x
-    return
-
-
-@app.cell
-def _(State, derivative, jnp, params):
-    _x = State(y=jnp.array(1.0), x=jnp.array(1.0))
-    derivative(params, jnp.array(1.0), _x)
-    return
-
-
-@app.cell
 def _(np, params, pl, u):
     df = pl.DataFrame(dict(
         t=np.linspace(0, 1, 11),
         x=np.array(u.x),
-        y=np.array(u.y),
+        y=np.array(u.y[:,1]),
     )).with_columns(
         y_acc=np.exp(pl.col.t*params.a),
     ).with_columns(
@@ -179,25 +166,30 @@ def _(alt, df):
 @app.cell
 def _(State, jax, jnp, params, solver):
     def loss(params, t, y0):
-        u, v = solver.solve(params, t, State(y=y0, x=jnp.sqrt(y0)))
-        return u.y[-1][0]
+        u, v, _ = solver.solve(params, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
+        return u.y[-1, 1]
 
-    jax.value_and_grad(loss, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array(1.0))
+    jax.value_and_grad(loss, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
     return
 
 
 @app.cell
 def _(State, jax, jnp, params, solver):
     def loss2(params, t, y0):
-        u, v = solver.solve(params, t, State(y=y0, x=jnp.sqrt(y0)))
+        u, v, _ = solver.solve(params, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
         return u.x[-1]
 
-    jax.value_and_grad(loss2, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array(1.0))
+    jax.value_and_grad(loss2, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
     return
 
 
 @app.cell
-def _():
+def _(State, jax, jnp, params, solver):
+    def loss3(params, t, y0):
+        u, v, _ = solver.solve(params, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
+        return v.y[-1, 1]
+
+    jax.value_and_grad(loss3, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
     return
 
 
