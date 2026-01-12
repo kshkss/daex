4d41faa5686b71e729d8062cafa009ded29e94b5
Akishika, Issui <kshkss@users.noreply.github.com>
2026-01-12 22:58:49 +0900

    test: update example code

--- example/example1.py
+++ example/example1.py
@@ -31,11 +31,23 @@ def _():
     import polars as pl
     import altair as alt
     from typing import NamedTuple
-    return NamedTuple, alt, daeint, def_semi_explicit_dae, jax, jnp, np, pl
+    from jax.flatten_util import ravel_pytree
+    return (
+        NamedTuple,
+        alt,
+        daeint,
+        def_semi_explicit_dae,
+        eqx,
+        jax,
+        jnp,
+        np,
+        pl,
+        ravel_pytree,
+    )
 
 
 @app.cell
-def _(NamedTuple, daeint, def_semi_explicit_dae, jax, jnp):
+def _(NamedTuple, daeint, def_semi_explicit_dae, eqx, jax, jnp):
     jax.config.update("jax_enable_x64", True)
 
     class State(NamedTuple):
@@ -48,18 +60,34 @@ def _(NamedTuple, daeint, def_semi_explicit_dae, jax, jnp):
     def derivative(params: Params, t: jax.Array, stat: State) -> State:
         return State(
             x=None,
-            y=stat.y * params.a,
+            y=stat.x * params.a * t,
         )
 
     def constraint(params: Params, t: jax.Array, stat: State) -> jax.Array:
         return stat.x ** 2 - jnp.sum(stat.y)
 
+    def analytic(params: Params, ts: jax.Array, stat0: State) -> State:
+        def result(params: Params, t: jax.Array, t0: jax.Array, stat0: State):
+            x = 0.25 * params.a * (t**2 - t0 ** 2) + jnp.sqrt(stat0.y)
+            return State(x=x, y=x**2)
+
+        u = jax.vmap(result, in_axes=(None, 0, None, None))(params, ts, ts[0], stat0)
+        v = jax.vmap(jax.jacfwd(result, argnums=1), in_axes=(None, 0, None, None))(params, ts, ts[0], stat0)
+
+        return u, eqx.tree_at(lambda v: v.x, v, replace=None)
+
     params = Params(a=jnp.array(1.0))
-    y0 = State(y=jnp.array([2.0, 1.0]), x=jnp.sqrt(3.0))
+    y0 = State(y=jnp.array(2.0), x=jnp.sqrt(2.0))
     dae = def_semi_explicit_dae(derivative, constraint, params, jnp.array(0.0), y0)
 
     u, v = daeint(params, dae, jnp.linspace(0, 1, 11), y0)
-    return Params, State, dae, params, u, v
+    return analytic, dae, params, u, v, y0
+
+
+@app.cell
+def _(analytic, jnp, params, y0):
+    analytic(params, jnp.linspace(0, 1, 11), y0)
+    return
 
 
 @app.cell
@@ -75,15 +103,30 @@ def _(v):
 
 
 @app.cell
-def _(np, params, pl, u):
+def _(analytic, jax, jnp, params, u, y0):
+    _u, _v = analytic(params, jnp.linspace(0, 1, 11), y0)
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), u, _u)
+    return
+
+
+@app.cell
+def _(analytic, jax, jnp, params, v, y0):
+    _u, _v = analytic(params, jnp.linspace(0, 1, 11), y0)
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), v, _v)
+    return
+
+
+@app.cell
+def _(analytic, jnp, np, params, pl, u, y0):
+    _acc, _ = analytic(params, jnp.linspace(0, 1, 11), y0)
+
     df = pl.DataFrame(dict(
         t=np.linspace(0, 1, 11),
         x=np.array(u.x),
-        y=np.array(u.y[:,1]),
+        y=np.array(u.y),
     )).with_columns(
-        y_acc=np.exp(pl.col.t*params.a),
-    ).with_columns(
-        x_acc=np.sqrt(pl.col.y_acc),
+        x_acc=np.array(_acc.x),
+        y_acc=np.array(_acc.y),
     )
     df
     return (df,)
@@ -168,69 +211,153 @@ def _(alt, df):
 
 
 @app.cell
-def _(State, dae, daeint, jax, jnp, params):
-    def loss(params, t, y0):
-        u, v = daeint(params, dae, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
-        return u.y[-1, 1]
+def _(dae, daeint, jax, jnp, params, y0):
+    def loss1(params, t, y0):
+        u, v = daeint(params, dae, t, y0)
+        return u.y[-1]
 
-    jax.value_and_grad(loss, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
-    return (loss,)
+    jax.grad(loss1, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss1,)
 
 
 @app.cell
-def _(State, dae, daeint, jax, jnp, params):
+def _(analytic, jax, jnp, params, y0):
+    def loss1_acc(params, t, y0):
+        u, v = analytic(params, t, y0)
+        return u.y[-1]
+
+    jax.grad(loss1_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss1_acc,)
+
+
+@app.cell
+def _(jax, jnp, loss1, loss1_acc, params, y0):
+    _u = jax.grad(loss1_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    _v = jax.grad(loss1, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), _u, _v)
+    return
+
+
+@app.cell
+def _(dae, daeint, jax, jnp, params, y0):
     def loss2(params, t, y0):
-        u, v = daeint(params, dae, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
+        u, v = daeint(params, dae, t, y0)
         return u.x[-1]
 
-    jax.value_and_grad(loss2, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
+    jax.grad(loss2, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss2,)
+
+
+@app.cell
+def _(analytic, jax, jnp, params, y0):
+    def loss2_acc(params, t, y0):
+        u, v = analytic(params, t, y0)
+        return u.x[-1]
+
+    jax.grad(loss2_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss2_acc,)
+
+
+@app.cell
+def _(jax, jnp, loss2, loss2_acc, params, y0):
+    _u = jax.grad(loss2_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    _v = jax.grad(loss2, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), _u, _v)
     return
 
 
 @app.cell
-def _(State, dae, daeint, jax, jnp, params):
+def _(dae, daeint, jax, jnp, params, y0):
     def loss3(params, t, y0):
-        u, v = daeint(params, dae, t, State(y=y0, x=jnp.sqrt(jnp.sum(y0))))
-        return v.y[-1, 1]
+        u, v = daeint(params, dae, t, y0)
+        return v.y[-1]
 
-    jax.value_and_grad(loss3, argnums=[0, 1, 2])(params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
+    jax.grad(loss3, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss3,)
+
+
+@app.cell
+def _(analytic, jax, jnp, params, y0):
+    def loss3_acc(params, t, y0):
+        u, v = analytic(params, t, y0)
+        return v.y[-1]
+
+    jax.grad(loss3_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    return (loss3_acc,)
+
+
+@app.cell
+def _(jax, jnp, loss3, loss3_acc, params, y0):
+    _u = jax.grad(loss3_acc, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    _v = jax.grad(loss3, argnums=[0, 1, 2])(params, jnp.linspace(-0.5, 1, 11), y0)
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), _u, _v)
     return
 
 
 @app.cell
-def _(jax, jnp, loss, params):
-    def hvp(f, x, v):
-        return jax.jvp(jax.grad(f), x, v)
+def _(jax, np):
+    def hvp(f, primals, tangents):
+      return jax.jvp(jax.grad(f, argnums=np.arange(len(primals))), primals, tangents)[1]
+    return (hvp,)
 
-    hvp(loss, (params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0])), (params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0])))
+
+@app.cell
+def _(hvp, jnp, loss1, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[0].set(1.0))
+
+    hvp(loss1, _primals, _tangents)
     return
 
 
 @app.cell
-def _(NamedTuple, Params, State, daeint, def_semi_explicit_dae, jax, jnp):
-    class _State(NamedTuple):
-        y: jax.Array
+def _(hvp, jnp, loss1_acc, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[0].set(1.0))
 
-    class _Params(NamedTuple):
-        a: jax.Array
+    hvp(loss1_acc, _primals, _tangents)
+    return
 
-    def _derivative(params: Params, t: jax.Array, stat: _State) -> State:
-        return _State(
-            y=stat.y * params.a,
-        )
 
-    def _constraint(params: Params, t: jax.Array, stat: _State) -> jax.Array:
-        return None
+@app.cell
+def _(hvp, jax, jnp, loss1, loss1_acc, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[0].set(1.0))
 
-    _params = _Params(a=jnp.array(1.0))
-    _dae = def_semi_explicit_dae(_derivative, _constraint, _params, jnp.array(0.0), _State(y=jnp.array([2.0, 1.0])))
-    _u, _v = daeint(_params, _dae, jnp.linspace(0, 1, 11), _State(y=jnp.array([2.0, 1.0])))
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), hvp(loss1, _primals, _tangents), hvp(loss1_acc, _primals, _tangents))
+    return
 
-    def _loss(params, t, y0):
-        u, v = daeint(params, _dae, t, _State(y=y0))
-        return u.y[-1, 1]
 
-    jax.value_and_grad(_loss, argnums=[0, 1, 2])(_params, jnp.linspace(0, 1, 11), jnp.array([2.0, 1.0]))
+@app.cell
+def _(hvp, jnp, loss1_acc, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[1].set(1.0))
+
+    hvp(loss1_acc, _primals, _tangents)
+    return
+
+
+@app.cell
+def _(hvp, jnp, loss1, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[1].set(1.0))
+
+    hvp(loss1, _primals, _tangents)
+    return
+
+
+@app.cell
+def _(hvp, jax, jnp, loss1, loss1_acc, params, ravel_pytree, y0):
+    _primals = (params, jnp.linspace(-0.5, 1, 11), y0)
+    _x, _unravel = ravel_pytree(_primals)
+    _tangents = _unravel(jnp.zeros_like(_x).at[1].set(1.0))
+
+    jax.tree.map(lambda a, b: jnp.allclose(a, b, 1e-4, 1e-4), hvp(loss1, _primals, _tangents), hvp(loss1_acc, _primals, _tangents))
     return
 
 
